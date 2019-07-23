e02743c290f46d77ed15e1753d7ca6d05538c413
Vasily93 <markovavasily@gmail.com>
2019-07-23 15:00:14 -0700

    created test for our post post  method

---
+++ test/posts.test.js
@@ -0,0 +1,24 @@
+const { getAgent } = require('../test/data-helpers');
+
+describe('it is a test for posts', () => {
+  it('our Post post test', async() => {
+    return getAgent()
+      .post('/api/v1/posts')
+      .send({
+        photoURL: 'a photo here',
+        caption: 'whatever',
+        tags: ['#tardygram', '#ilovemycats']
+
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),  
+          username: expect.any(String),
+          photoURL: 'a photo here',
+          caption: 'whatever',
+          tags: ['#tardygram', '#ilovemycats'],
+          __v: 0
+        });
+      });
+  });
+});
