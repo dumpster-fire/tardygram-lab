9f34e23d6dde9ea964d36d240690b312c0a8360c
Vasily93 <markovavasily@gmail.com>
2019-07-23 14:58:36 -0700

    created file and filled it in

---
+++ test/data-helpers.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const request = require('supertest');
+const connect = require('../lib/utils/connect');
+const app = require('../lib/app');
+const seedData = require('./seed-data');
+
+const prepare = arr => JSON.parse(JSON.stringify(arr));
+
+beforeAll(() => {
+  connect();
+});
+
+beforeEach(() => {
+  return mongoose.connection.dropDatabase();
+});
+
+let agent = request.agent(app);
+let seededUsers = null;
+let seededPosts = null;
+beforeEach(async() => {
+  const { users, posts } = await seedData();
+  seededUsers = prepare(users);
+  seededPosts = prepare(posts);
+
+  return await agent
+    .post('/api/v1/signin')
+    .send({ username: users[0].username, email: users[0].email, password: 'password' });
+});
+
+afterAll(() => {
+  return mongoose.connection.close();
+});
+
+module.exports = {
+  getAgent: () => agent,
+  getUsers: () => seededUsers,
+  getPosts: () => seededPosts
+};
+
+
