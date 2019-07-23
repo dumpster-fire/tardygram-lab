1bc52335228daabcc80d21134c0be89668faa25b
Vasily93 <markovavasily@gmail.com>
2019-07-23 15:55:19 -0700

    test for get all method

--- test/posts.test.js
+++ test/posts.test.js
@@ -1,7 +1,7 @@
-const { getAgent } = require('../test/data-helpers');
+const { getAgent, getPosts } = require('../test/data-helpers');
 
 describe('it is a test for posts', () => {
-  it('our Post post test', async() => {
+  it('creates a post', async() => {
     return getAgent()
       .post('/api/v1/posts')
       .send({
@@ -17,6 +17,18 @@ describe('it is a test for posts', () => {
           caption: 'whatever',
           tags: ['#tardygram', '#ilovemycats'],
           __v: 0
+        });
+      });
+  });
+
+  it('gets all posts', () => {
+    const posts = getPosts();
+
+    return getAgent()
+      .get('/api/v1/posts')
+      .then(res => {
+        posts.forEach(post => {
+          expect(res.body).toContainEqual(post);
         });
       });
   });
