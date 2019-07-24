88362fc73e1d0bd2cc9031939fdb71fdb00afa29
Vasily93 <markovavasily@gmail.com>
2019-07-24 15:01:04 -0700

    test and route to delete a comment

--- test/comments.test.js
+++ test/comments.test.js
@@ -1,10 +1,9 @@
-const { getAgent, getUsers, getPosts } = require('./data-helpers');
+const { getAgent, getUsers, getPosts, getComments } = require('./data-helpers');
 
 describe('it is a test for comments', () => {
   it('creates a comment', async() => {
     const user = getUsers()[0];
     const post = getPosts()[0];
-    console.log(user);
     return getAgent()
       .post('/api/v1/comments')
       .send({
@@ -20,6 +19,18 @@ describe('it is a test for comments', () => {
           post: expect.any(String),
           __v: 0
         });
+      });
+  });
+
+  it('can delete a comment', () => {
+    const comments = getComments();
+    const stringifiedComments = JSON.parse(JSON.stringify(comments));
+    const comment = stringifiedComments[0];
+
+    return getAgent()
+      .delete(`/api/v1/comments/${comment._id}`)
+      .then(res => {
+        expect(res.body).toEqual(comment);
       });
   });
 });
